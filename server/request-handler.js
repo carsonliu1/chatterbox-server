fa4fb65b5a516491d937a75e75e9234facf1d1d5
Carson <liucarson@yahoo.com>
2022-06-17 17:37:33 -0700

    created post and get reponses

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,18 +1,43 @@
-/*************************************************************
+var content = []
 
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept, authorization',
+  'access-control-max-age': 10 // Seconds.
+};
 
 var requestHandler = function(request, response) {
+  // The outgoing status.
+  var statusCode = 200;
+  // See the note below about CORS headers.
+  var headers = defaultCorsHeaders
+  // Tell the client we are sending them plain text.
+
+  // You will need to change this if you are sending something
+  // other than plain text, like JSON or HTML.
+  headers['Content-Type'] = 'application/json';
+
+  if (request.url === '/classes/messages') {
+    if (request.method === 'GET') {
+      response.writeHead(statusCode, headers)
+      response.end(JSON.stringify(content))
+    } else if (request.method === 'POST') {
+      let message = ''
+      request.on('data', (data) => {
+        message += data.toString()
+      })
+      request.on('end', () => {
+        let messageObj = JSON.parse(message)
+        content.push(messageObj)
+        response.writeHead(201, headers)
+        response.end(JSON.stringify(content))
+      })
+    }
+  } else {
+    response.writeHead(404, headers)
+    response.end()
+  }
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -27,32 +52,8 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  // console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
-  // The outgoing status.
-  var statusCode = 200;
-
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end('Hello, World!');
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -64,9 +65,6 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept, authorization',
-  'access-control-max-age': 10 // Seconds.
-};
+
+
+exports.requestHandler = requestHandler
